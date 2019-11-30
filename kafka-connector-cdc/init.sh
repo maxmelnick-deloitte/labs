ec964838629a5baed5a0a9f06302d39b673c8dcd
Max Melnick <mmelnick@deloitte.com>
2019-11-30 15:55:14 -0500

    less verbose spark logging

--- kafka-connector-cdc/init.sh
+++ kafka-connector-cdc/init.sh
@@ -9,3 +9,8 @@ tar xzf kafka-connect-dse.tar.gz
 wget http://apache-mirror.8birdsvideo.com/spark/spark-2.4.4/spark-2.4.4-bin-hadoop2.7.tgz
 
 tar xzf spark-2.4.4-bin-hadoop2.7.tgz
+
+cp spark-2.4.4-bin-hadoop2.7/conf/log4j.properties.template spark-2.4.4-bin-hadoop2.7/conf/log4j.properties
+
+echo "log4j.logger.org.apache.spark=WARN" >> spark-2.4.4-bin-hadoop2.7/conf/log4j.properties
+echo "log4j.logger.org.apache.kafka=WARN" >> spark-2.4.4-bin-hadoop2.7/conf/log4j.properties
